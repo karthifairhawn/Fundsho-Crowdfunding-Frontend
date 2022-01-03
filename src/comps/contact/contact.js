821515a90e1471bbd7c42973916c229649be599e
Karthi Fair Hawn <karthifairhawn@gmail.com>
2022-01-03 14:58:16 +0530

    contact us page

--- src/comps/contact/contact.js
+++ src/comps/contact/contact.js
@@ -9,25 +9,47 @@ class Contact extends React.Component{
     render(){
         return(  
             
-            <>
+            <div className="contact-us-page">
             <Navbar/>
-                <div className="contact-us">
-                    <div className="title-cu">EMAILUS</div>
-                    <form className="form-cu">
-                        <div className="name-email-cu">
-                            <input type="text" name="name" placeholder="Your Name"/>
-                            <input type="email" name="email" id="email" placeholder="Your Email"/>
+            <section id="contact">
+                <div className="contact-box">
+                    <div className="contact-links">
+                    <h2>CONTACT</h2>
+                    <div className="links">
+                        <div className="link">
+                        <a><img src="https://i.postimg.cc/m2mg2Hjm/linkedin.png" alt="linkedin"/></a>
                         </div>
-                        <div className="your-message-cu">
-                            <textarea name="message" id="" cols="30" rows="10"></textarea>
+                        <div className="link">
+                        <a><img src="https://i.postimg.cc/YCV2QBJg/github.png" alt="github"/></a>
                         </div>
-                        <div className="submit-btn-cu">
-                            <input type="submit" value="Send Message"/>
+                        <div className="link">
+                        <a><img src="https://i.postimg.cc/W4Znvrry/codepen.png" alt="codepen"/></a>
                         </div>
+                        <div className="link">
+                        <a><img src="https://i.postimg.cc/NjLfyjPB/email.png" alt="email"/></a>
+                        </div>
+                    </div>
+                    </div>
+                    <div className="contact-form-wrapper">
+                    <form>
+                        <div className="form-item">
+                        <input type="text" name="sender" required/>
+                        <label>Name:</label>
+                        </div>
+                        <div className="form-item">
+                        <input type="text" name="email" required/>
+                        <label>Email:</label>
+                        </div>
+                        <div className="form-item">
+                        <textarea className="" name="message" required></textarea>
+                        <label>Message:</label>
+                        </div>
+                        <button className="submit-btn">Send</button>  
                     </form>
+                    </div>
                 </div>
-                <hr />  
-            </>         
+            </section>
+            </div>         
         );
     }
 }
