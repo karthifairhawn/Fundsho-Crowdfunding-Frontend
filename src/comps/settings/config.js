cef894b6943e71d53e6b56e88a65fcbf6e44d5ce
Karthi Fair Hawn <karthifairhawn@gmail.com>
2022-04-25 18:05:38 +0530

    local db to cloud DB

--- src/comps/settings/config.js
+++ src/comps/settings/config.js
@@ -3,6 +3,6 @@
  
 export const APIIP = {
     // ip :"http://52d3-2409-4072-6011-3855-31ce-366a-4ce8-9ce5.ngrok.io"
-    ip :"http://localhost:8080"
+    ip :"https://fundshop-api.herokuapp.com"
     // ip:"http://192.168.43.114:8080"
 };
